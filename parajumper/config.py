f1b4bf51c6a771e8adfb445c4ac64d36b5771eb6
Haoyang Xu <Haoyang Xu>
2016-11-24 14:56:10 +0800

    check and init config file

--- parajumper/config.py
+++ parajumper/config.py
@@ -9,6 +9,28 @@ If still nothing, a default config file is written.
 """
 import os
 
+DEFAULT_CONFIG_PATH = os.environ['HOME'] + '/.config/parajumper'
+
+CONF_DEFAULT = """
+---
+author: Default ParaJumper
+name: My ParaJumper Note
+---
+This is default configuration for ParaJumper.
+Feel free to change it.
+
+"""
+
 def check_config():
     """Check if the config is available using os module."""
-    pass
+    try:
+        os.listdir(DEFAULT_CONFIG_PATH)
+    except OSError:
+        os.mkdir(DEFAULT_CONFIG_PATH)
+    try:
+        conffile = open(DEFAULT_CONFIG_PATH + '/config.yaml')
+    except OSError:
+        conffile = open(DEFAULT_CONFIG_PATH + '/config.yaml', 'w+')
+        print(CONF_DEFAULT, file=conffile)
+        conffile.close()
+    return conffile
