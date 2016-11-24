7d52ec403f5900b59d09ab61069e23e64af0059d
Haoyang Xu <Haoyang Xu>
2016-11-24 10:03:00 +0800

    add requirements.txt

---
+++ parajumper/config.py
@@ -0,0 +1,9 @@
+"""
+Process config file, which is a yaml file.
+
+The default position of the file is at ~/.config/parajumper/config.yaml.
+
+If the default file is not found, it falls back to ~/.config/parajumper/<any-name>.yaml.
+
+If still nothing, a default config file is written.
+"""
