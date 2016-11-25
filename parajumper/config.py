075abc6ea590923f2c9d26b662561594fea4c482
Haoyang Xu <Haoyang Xu>
2016-11-25 11:59:53 +0800

    make correct yaml document

--- parajumper/config.py
+++ parajumper/config.py
@@ -8,15 +8,14 @@ If the default file is not found, it falls back to ~/.config/parajumper/<any-nam
 If still nothing, a default config file is written.
 """
 import os
+import yaml
 
 DEFAULT_CONFIG_PATH = os.environ['HOME'] + '/.config/parajumper'
 
-CONF_DEFAULT = """---
-author: Default ParaJumper
+CONF_DEFAULT = """author: Default ParaJumper
 name: My ParaJumper Note
----
-This is default configuration for ParaJumper.
-Feel free to change it. """
+# This is default configuration for ParaJumper.
+# Feel free to change it. """
 
 def check_config():
     """Check if the config is available using os module."""
@@ -29,5 +28,16 @@ def check_config():
     except OSError:
         conffile = open(DEFAULT_CONFIG_PATH + '/config.yaml', 'w+')
         print(CONF_DEFAULT, file=conffile)
+    finally:
         conffile.close()
-    return conffile
+    return conffile.name
+
+def read_config(f):
+    """Read config into a dictionary.
+
+    f: file name returned by check_config() or supplied in func call."""
+    document = ''
+    with open(f) as cf:
+        for line in cf:
+            document += line
+    return yaml.load(document)
