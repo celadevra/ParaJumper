f7310d51313cfc453b8a8c619160e96514bf06ca
Haoyang Xu <haoyang@fastmail.com>
2016-11-29 22:23:14 +0800

    tests for setting tags

--- tests/item_test.py
+++ tests/item_test.py
@@ -15,3 +15,19 @@ def test_get_item_type():
     assert new_item.type == 'event'
     new_item = Item(bullet='19', content='Test')
     assert new_item.type == 'notes'
+
+def test_set_tags():
+    """test if tags can be set correctly."""
+    new_item = Item(bullet='3', content='测试')
+    assert new_item.tags == []
+    new_item.set_tags('play', 'test')
+    assert new_item.tags == ['play', 'test']
+    new_item.set_tags(['foo', 'bar'])
+    assert new_item.tags == ['bar', 'foo']
+    new_item.set_tags({'a':1, 'b':2})
+    assert new_item.tags == ['a', 'b']
+    new_item.set_tags(135, '246')
+    assert new_item.tags == ['135', '246']
+    new_item.set_tags((50, 21))
+    assert new_item.tags == ['21', '50']
+#TODO: test for unicode cases
