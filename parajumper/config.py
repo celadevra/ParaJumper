ed6dfbf437f26001c2e7bd9c9ded9720533a66fd
Haoyang Xu <haoyang@fastmail.com>
2016-11-29 10:22:40 +0800

    meaningful var and arg names

--- parajumper/config.py
+++ parajumper/config.py
@@ -53,39 +53,39 @@ class Config():
                 document += line
             self.options = yaml.load(document)
 
-    def update_config(self, f=DEFAULT_CONFIG_FILE, df=DEFAULT_CONFIG_FILE):
+    def update_config(self, read_f=DEFAULT_CONFIG_FILE, write_f=DEFAULT_CONFIG_FILE):
         """Read config from a file into a dictionary.
         Then merge it with the existing directory.
 
-        f: file name returned by check_config() or supplied in func call.
-        df: config file to write to."""
+        read_f: file name returned by check_config() or supplied in func call.
+        write_f: config file to write to."""
         document = ''
-        cf = open(f)
-        for line in cf:
+        conf_file = open(read_f)
+        for line in conf_file:
             document += line
-        cf.close()
+        conf_file.close()
         new_conf = yaml.load(document)
         self.options.update(new_conf)
-        cf = open(df, 'w')
-        yaml.dump(self.options, cf, default_flow_style=False)
-        cf.close()
+        conf_file = open(write_f, 'w')
+        yaml.dump(self.options, conf_file, default_flow_style=False)
+        conf_file.close()
 
-    def update_items(self, d, f=DEFAULT_CONFIG_FILE):
+    def update_items(self, dict_conf_item, conf_f=DEFAULT_CONFIG_FILE):
         """add/change items in the config.
 
-        d: a dict recording things to be added/changed in the conf.
-        f: file to write config to."""
-        self.options.update(d)
-        cf = open(f, 'w+')
-        yaml.dump(self.options, cf, default_flow_style=False)
-        cf.close()
+        dict_conf_item: a dict recording things to be added/changed in the conf.
+        conf_f: file to write config to."""
+        self.options.update(dict_conf_item)
+        conf_file = open(conf_f, 'w+')
+        yaml.dump(self.options, conf_file, default_flow_style=False)
+        conf_file.close()
 
-    def remove(self, k, f=DEFAULT_CONFIG_FILE):
+    def remove(self, k, conf_f=DEFAULT_CONFIG_FILE):
         """remove specified key and its value from conf dict.
 
         k: key to remove.
-        f: file to write config to."""
+        conf_f: file to write config to."""
         del self.options[k]
-        cf = open(f, 'w+')
-        yaml.dump(self.options, cf, default_flow_style=False)
-        cf.close()
+        conf_file = open(conf_f, 'w+')
+        yaml.dump(self.options, conf_file, default_flow_style=False)
+        conf_file.close()
