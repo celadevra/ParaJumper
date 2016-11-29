6e478223a3299fc002d5d75c91656c3b2cdda1ac
Haoyang Xu <haoyang@fastmail.com>
2016-11-29 10:37:54 +0800

    tests for save function

--- tests/config_test.py
+++ tests/config_test.py
@@ -10,16 +10,16 @@ CONF_FILE = os.environ['HOME'] + '/.config/parajumper/config.yaml'
 def test_config_creation(empty_config):
     """test if config can be created when absent"""
     assert not os.access(CONF_FILE, os.F_OK)
-    d = Config()
+    conf = Config()
     assert os.access(CONF_FILE, os.R_OK)
-    assert d.options['author'] == 'Default ParaJumper'
+    assert conf.options['author'] == 'Default ParaJumper'
 
 def test_config_content():
     """test if config's content is the expected default content."""
     line_count = 0
     word = ''
-    with open(CONF_FILE) as f:
-        for line in f:
+    with open(CONF_FILE) as conf:
+        for line in conf:
             line_count += 1
             if line_count == 1:
                 word = line.split()[0]
@@ -33,6 +33,16 @@ def test_read_config():
     assert conf.options['author'] == 'Default ParaJumper'
     assert conf.options['name'] == 'My ParaJumper Note'
 
+def test_save_config():
+    """test if the config is sync with file."""
+    Config()
+    assert os.access(CONF_FILE, os.R_OK)
+    lines = []
+    with open(CONF_FILE) as conf_file:
+        for line in conf_file:
+            lines.append(line)
+    assert lines[0].split()[0] == 'author:'
+
 def test_add_config():
     """test inserting new items into the config."""
     conf = Config()
@@ -44,4 +54,4 @@ def test_remove_config():
     conf = Config()
     conf.update_items({'foo': 'bar'})
     conf.remove('foo')
-    assert not 'foo' in conf.options.keys() 
+    assert not 'foo' in conf.options.keys()
