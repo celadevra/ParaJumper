38fff92c56b8778fc4a4fd3b5b7a83fcf81f72f3
Haoyang Xu <haoyang@fastmail.com>
2016-12-01 21:33:38 +0800

    remove unwanted fixture call

--- tests/item_test.py
+++ tests/item_test.py
@@ -3,14 +3,14 @@
 import datetime
 from parajumper.item import Item
 
-def test_create_item(empty_db):
+def test_create_item():
     """Test creation of item."""
     new_item = Item(bullet='.', content='# Test new item')
     assert new_item
     assert new_item.type == 'todo'
     assert new_item.content == '# Test new item'
 
-def test_get_item_type(empty_db):
+def test_get_item_type():
     """test if item type can be obtained correctly."""
     new_item = Item(content='# Test item')
     assert new_item.type == 'event'
@@ -22,7 +22,7 @@ def test_show_item():
     new_item = Item(content="Content.", bullet="*")
     assert str(new_item) == "* Content.\ntags: []\nCreated: %s\nUpdated: N/A" % str(datetime.date.today())
 
-def test_update_item(empty_db):
+def test_update_item():
     """test if items whose attributes are changed have different timestamp."""
     new_item = Item(bullet='.', content='Bring milk home')
     new_item.update(content='Bring peanuts home.')
@@ -33,7 +33,7 @@ def test_update_item(empty_db):
     time2 = datetime.datetime.strptime(new_item.create_date, '%Y-%m-%d %H:%M:%S.%f')
     assert time1 - time2 > datetime.timedelta(0)
 
-def test_unicode_content(empty_db):
+def test_unicode_content():
     """Test if content unicode is handled correctly."""
     new_item = Item(bullet='o', content='你好お元気ですか')
     new_item.update(content=new_item.content[:2])
