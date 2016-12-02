ee8bf9d41bb9dba1b7b27c45f4e56fa471f65b48
Haoyang Xu <haoyang@fastmail.com>
2016-12-02 13:52:36 +0800

    test for show action of binder

--- tests/binder_test.py
+++ tests/binder_test.py
@@ -4,8 +4,16 @@ longer time period forms a binder, so do all items with a certain tag, or
 search result from a query."""
 
 from parajumper.binder import Binder
+import parajumper.db as db
 
 def test_create_binder():
     """Test create method of binder class."""
     binder = Binder()
     assert binder.members is None
+    assert binder.name == 'binder'
+    assert binder.kind == 'adhoc'
+
+def test_show_binder():
+    """Test printing binder content."""
+    binder = Binder()
+    assert str(binder) == '%s binder: %s\n' % (binder.kind, binder.name)
