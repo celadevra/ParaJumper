aba0c47348b3ee8024d5281acd3ea8a6d2d6a903
Haoyang Xu <haoyang@fastmail.com>
2016-12-08 20:53:17 +0800

    sort of token list in sane place

--- parajumper/indices.py
+++ parajumper/indices.py
@@ -2,20 +2,26 @@
 import jieba
 import parajumper.db
 
-def _get_freq(word, text):
-    """Get number of occurrence of word in text."""
-    word = word.lower()
-    text = text.lower()
-    return text.count(word)
+def _get_freq(word_list):
+    """Get number of occurrence of word in a list."""
+    counts = dict()
+    for word in word_list:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    results = []
+    for word in counts:
+        results.append((counts[word], word))
+    results.sort()
+    return results
 
 def gen_index(identity, text):
     """Generate a content index for item with id = identity."""
     words = [w for w in jieba.lcut_for_search(text.lower()) if w != " "]
-    word_freq = []
+    word_freq = _get_freq(words)
     result = []
-    for word in words:
-        word_freq.append((_get_freq(word, text), word))
-    word_freq.sort()
     for (_, word) in word_freq:
         result.append(word)
     parajumper.db.INDEX_T.update_one({"identity": identity},
