711fa18f6ff906ecca7644f86e5a7dc862848b32
Haoyang Xu <haoyang@fastmail.com>
2016-12-11 12:22:33 +0800

    barebone cli entry point

---
+++ parajumper/cli/main.py
@@ -0,0 +1,26 @@
+"""ParaJumper main application entry point."""
+from cement.core.exc import CaughtSignal
+from cement.core.foundation import CementApp
+
+class PJApp(CementApp):
+    """Cement app class for PJ"""
+    label = 'pj'
+
+APP = PJApp()
+
+def main():
+    """PJ's main entry function"""
+    try:
+        code = 0
+        APP.run()
+    except CaughtSignal as error:
+        code = 0
+        print(error)
+    except Exception as error:
+        code = 1
+        print(error)
+
+    APP.close(code)
+
+if __name__ == '__main__':
+    main()
