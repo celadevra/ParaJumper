814b77f788bd59132393cb848c98b271eb376d0b
Haoyang Xu <haoyang@fastmail.com>
2016-12-11 12:29:52 +0800

    correctly specify deps version

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     description="Command line journal/note-taking tool.",
     version=parajumper.__version__,
     entry_points={
-        'console_scripts': ['parajumper.cli.main:main']},
+        'console_scripts': ['parajumper = parajumper.cli.main:main']},
     install_requires=[
         # dev reqs
         # 'pylint',
         # 'pytest',
-        'cement == 2.10',
-        'clint == 0.5',
+        'cement >= 2.10',
+        'clint >= 0.5',
         'jieba',
-        'pymongo == 3.4',
+        'pymongo >= 3.4',
         'ruamel.yaml'])
