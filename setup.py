d24f8d2db60ef217ffc12739771885af466f5ca7
Haoyang Xu <haoyang@fastmail.com>
2016-12-11 12:08:45 +0800

    add package desc

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import parajumper
 
 setup(
     name="parajumper",
+    description="Command line journal/note-taking tool.",
     version=parajumper.__version__,
     entry_points={
         'console_scripts': ['parajumper.cli.main']},
