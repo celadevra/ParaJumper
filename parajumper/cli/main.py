d86eb4a969d02678ee848a02b2af5a4c78518124
Haoyang Xu <haoyang@fastmail.com>
2016-12-11 13:16:19 +0800

    add arg handler
    app now accepts --help arg

--- parajumper/cli/main.py
+++ parajumper/cli/main.py
@@ -1,18 +1,29 @@
 """ParaJumper main application entry point."""
 from cement.core.exc import CaughtSignal
 from cement.core.foundation import CementApp
+from cement.ext.ext_argparse import ArgParseArgumentHandler
+
+class PJArgHandler(ArgParseArgumentHandler):
+    class Meta:
+        label = 'pj_args_handler'
+
+        def error(self, message):
+            """Error handling for arg parsing"""
+            super(PJArgHandler, self).error("unknown args")
 
 class PJApp(CementApp):
     """Cement app class for PJ"""
     class Meta:
         """Metadata for PJApp class"""
         label = 'pj'
+        arg_handler = PJArgHandler
 
 APP = PJApp()
 
 def main():
     """PJ's main entry function"""
     try:
+        APP.setup()
         code = 0
         APP.run()
     except CaughtSignal as error:
