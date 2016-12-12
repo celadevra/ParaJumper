70fafd5450623a8398e1697783b7caa44d4b5832
Haoyang Xu <haoyang@fastmail.com>
2016-12-12 18:20:05 +0800

    fixtures to set and restore test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 
 import os
 import pytest
+from parajumper.config import Config
 from parajumper.db import CLIENT
 
 @pytest.fixture(scope="module")
@@ -13,10 +14,30 @@ def empty_config():
     except OSError:
         return
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
+def db_config():
+    """Make db-related tests use test database."""
+    conf = Config()
+    conf.update_items({'database': {'db_name': 'test',
+                                    'kind': 'mongodb',
+                                    'location': 'mongodb://localhost:27017'}})
+    print("Setting db to test")
+    yield conf
+
+@pytest.fixture()
+def db_teardown():
+    """Restore db to production env."""
+    conf = Config()
+    conf.update_items({'database': {'db_name': 'pj',
+                                    'kind': 'mongodb',
+                                    'location': 'mongodb://localhost:27017'}})
+    print("Setting db to pj")
+    yield conf
+
+@pytest.fixture()
 def empty_db():
     """empty test database."""
-    database = CLIENT.pj
+    database = CLIENT['test']
     items = database.items
     items.drop()
     binders = database.binders
