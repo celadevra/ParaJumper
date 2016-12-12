1d4f332b0489733075826dd4f1145d1793361b1a
Haoyang Xu <haoyang@fastmail.com>
2016-12-12 15:58:19 +0800

    implement help and command err handling

--- parajumper/cli/main.py
+++ parajumper/cli/main.py
@@ -1,39 +1,25 @@
 """ParaJumper main application entry point."""
-from cement.core.exc import CaughtSignal
-from cement.core.foundation import CementApp
-from cement.ext.ext_argparse import ArgParseArgumentHandler
-
-class PJArgHandler(ArgParseArgumentHandler):
-    class Meta:
-        label = 'pj_args_handler'
-
-        def error(self, message):
-            """Error handling for arg parsing"""
-            super(PJArgHandler, self).error("unknown args")
-
-class PJApp(CementApp):
-    """Cement app class for PJ"""
-    class Meta:
-        """Metadata for PJApp class"""
-        label = 'pj'
-        arg_handler = PJArgHandler
-
-APP = PJApp()
+from clint.arguments import Args
+from clint.textui import puts, indent, colored
+import parajumper.cli.help as printhelp
 
 def main():
     """PJ's main entry function"""
+    commands_list = ['help', 'today',
+                     'reschedule', 'pin',
+                     'tag', 'export',
+                     'new', 'move', 'edit', 'del', 'delete']
+    args = Args()
+    if len(args) == 0:
+        args = Args(['today'])
     try:
-        APP.setup()
-        code = 0
-        APP.run()
-    except CaughtSignal as error:
-        code = 0
-        print(error)
-    except Exception as error:
-        code = 1
-        print(error)
-
-    APP.close(code)
-
+        command = commands_list.index(args[0])
+    except ValueError:
+        command = 0
+        puts(colored.red("Unknown command: %s" % args[0]))
+    if command == 0: # help
+        if (not args.has(1)) or (args.not_flags[1] not in commands_list):
+            printhelp.default_help()
+            
 if __name__ == '__main__':
     main()
