c57f77df79f510dafcb20040270826cc76dfb45a
Haoyang Xu <haoyang@fastmail.com>
2016-12-13 14:18:52 +0800

    additional help text

--- parajumper/cli/help.py
+++ parajumper/cli/help.py
@@ -1,7 +1,17 @@
 """Show help to various commands and arguments of ParaJumper."""
 
-from clint.textui import puts, indent
+from clint.textui import puts, indent, colored
 
 def default_help():
     """Default help message, invoked with 'pj help' or 'pj --help'."""
     puts("Usage: pj [command [<binder selector>] [<item selector>]]")
+    puts("")
+    puts("Supported commands:")
+    with indent(4):
+        puts(colored.yellow("new"))
+    with indent(6):
+        puts("Create new items and binders.")
+    with indent(4):
+        puts(colored.yellow("today"))
+    with indent(6):
+        puts("Default command. Show today's binder.")
