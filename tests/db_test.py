75dd6d9b0353b5e467c0f1add49dc63073c0e9c9
Haoyang Xu <haoyang@fastmail.com>
2016-12-13 19:01:33 +0800

    use conf obj in tests

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,13 +1,15 @@
 """Tests for database operations."""
 from parajumper.item import Item, ITEMS_DICT
 from parajumper.binder import Binder, BINDERS_DICT
+from parajumper.config import DBConfig
 import parajumper.db as db
 
 def test_save_item(empty_db):
     """Test for saving item."""
+    conf = DBConfig()
     item = Item(content="Test content", bullet=".", tags=['oper'])
     db.save_item(item)
-    assert db.ITEM_T.find_one({"content":"Test content"})['tags'] == ['oper'] is not None
+    assert conf.item_t.find_one({"content":"Test content"})['tags'] == ['oper'] is not None
 
 def test_load_item(empty_db):
     """Test for loading item."""
@@ -19,33 +21,36 @@ def test_load_item(empty_db):
 
 def test_removing_item(empty_db):
     """Test for removing item."""
+    conf = DBConfig()
     item = Item(content="Test content", bullet=".", tags=['oper'])
     identity = db.save_item(item)
     db.remove_item(identity)
-    assert db.ITEM_T.find_one({"identity": identity}) is None
+    assert conf.item_t.find_one({"identity": identity}) is None
 
 def test_save_binder_and_members(empty_db):
     """Test for saving binder and its members. Items not in the
     binder should not be saved."""
+    conf = DBConfig()
     item1 = Item(content="I'm in a binder.")
     item2 = Item(content="Not in a binder.")
     binder = Binder()
     binder.add_members(item1)
     identity = db.save_binder(binder)
-    assert db.ITEM_T.find_one({"content":"I'm in a binder."}) is not None
-    assert db.BINDER_T.find_one({"identity":identity}) is not None
-    assert db.ITEM_T.find_one({"content":item2.content}) is None
+    assert conf.item_t.find_one({"content":"I'm in a binder."}) is not None
+    assert conf.binder_t.find_one({"identity":identity}) is not None
+    assert conf.item_t.find_one({"content":item2.content}) is None
 
 def test_removing_binder(empty_db):
     """Test for removing binder from db. Items in the binder will be reserved."""
+    conf = DBConfig()
     item1 = Item(content="This is test of 12-03")
     binder = Binder()
     binder.add_members(item1)
     item_identity = db.save_item(item1)
     binder_identity = db.save_binder(binder)
     db.remove_binder(binder_identity)
-    assert db.BINDER_T.find_one({"identity": binder_identity}) is None
-    assert db.ITEM_T.find_one({"identity": item_identity})
+    assert conf.binder_t.find_one({"identity": binder_identity}) is None
+    assert conf.item_t.find_one({"identity": item_identity})
 
 def test_loading_binder(empty_db):
     """Test for loading binder from db."""
@@ -59,7 +64,8 @@ def test_loading_binder(empty_db):
 
 def test_indices_order(empty_db):
     """Test if indices are ordered by frequency."""
+    conf = DBConfig()
     item1 = Item(content="Do you call this this and that also this?")
     db.save_item(item1)
-    wordlist = db.INDEX_T.find_one({"identity":item1.identity})['words']
+    wordlist = conf.index_t.find_one({"identity":item1.identity})['words']
     assert wordlist[-1] == 'this'
