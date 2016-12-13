beeb2e507ce10395376189981bc415c6460b14da
Haoyang Xu <haoyang@fastmail.com>
2016-12-13 19:43:15 +0800

    db config is always updated for db tests

--- parajumper/indices.py
+++ parajumper/indices.py
@@ -1,6 +1,7 @@
 """Work with locally generated indices."""
 import jieba
 import parajumper.db
+import parajumper.config
 
 def _get_freq(word_list):
     """Get number of occurrence of word in a list."""
@@ -13,11 +14,12 @@ def _get_freq(word_list):
 
 def gen_index(identity, text):
     """Generate a content index for item with id = identity."""
+    conf = parajumper.config.DBConfig()
     words = [w for w in jieba.cut_for_search(text.lower()) if w != " "]
     word_freq = _get_freq(words)
     result = []
     for (_, word) in word_freq:
         result.append(word)
-    parajumper.db.INDEX_T.update_one({"identity": identity},
-                                     {"$set": {"words": result}},
-                                     upsert=True)
+    conf.index_t.update_one({"identity": identity},
+                            {"$set": {"words": result}},
+                            upsert=True)
