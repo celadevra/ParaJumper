486ab927c421ad318a70e7584211a74c8822d834
Haoyang Xu <haoyang@fastmail.com>
2016-12-14 16:10:07 +0800

    use item's own show func to display item

--- parajumper/cli/today.py
+++ parajumper/cli/today.py
@@ -1,10 +1,19 @@
 """Deal with 'today' command."""
 
 import datetime
-from parajumper.db import create_date_binder
+from clint.textui import puts
+from parajumper.db import create_date_binder, load_item
 
 def show():
     """Show today's binder."""
     today = str(datetime.date.today())
     binder = create_date_binder(today)
     print(binder)
+    items = []
+    index = 0
+    for member in binder.members:
+        items.append(load_item(member))
+    for item in items:
+        puts("[%2d]" % index)
+        print(item)
+        index += 1
