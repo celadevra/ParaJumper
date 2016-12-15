685a8949f3fd8a655316500f90ae3697c9a9b09f
Haoyang Xu <haoyang@fastmail.com>
2016-12-15 14:36:10 +0800

    deal with verbosity levels

--- parajumper/cli/today.py
+++ parajumper/cli/today.py
@@ -4,7 +4,18 @@ import datetime
 from clint.textui import puts, colored
 from parajumper.db import create_date_binder, load_item
 
-def show():
+def dispatch(args):
+    """Dispatch and control command behaviours according to args."""
+    v_level = 0
+    if '-v' in args.flags:
+        v_level = 1
+    if '-vv' in args.flags:
+        v_level = 2
+    if '-vvv' in args.flags:
+        v_level = 3
+    return show(v_level)
+
+def show(level=0):
     """Show today's binder."""
     today = str(datetime.date.today())
     binder = create_date_binder(today)
@@ -15,5 +26,5 @@ def show():
         items.append(load_item(member))
     for item in items:
         puts(colored.blue("[%2d]" % index))
-        print(item.__str__(verbose=0))
+        print(item.__str__(verbose=level))
         index += 1
