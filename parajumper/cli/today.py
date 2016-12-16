20b2b6c44335a85023299e7c47de4173454bac6a
Haoyang Xu <haoyang@fastmail.com>
2016-12-16 16:12:47 +0800

    handle date from/to and ago args

--- parajumper/cli/today.py
+++ parajumper/cli/today.py
@@ -7,19 +7,37 @@ from parajumper.db import create_date_binder, load_item
 def dispatch(args):
     """Dispatch and control command behaviours according to args."""
     v_level = 0
+    date_to = None
+    date_from = None
+    ago = None
+    today = True
     if '-v' in args.flags:
         v_level = 1
     if '-vv' in args.flags:
         v_level = 2
     if '-vvv' in args.flags:
         v_level = 3
-    return show(v_level)
+    if '-df' in args.flags:
+        date_from = args.value_after('-df')
+        today = False
+    if '-dt' in args.flags:
+        date_to = args.value_after('-dt')
+        today = False
+    if '-da' in args.flags:
+        ago = args.value_after('-da')
+        today = False
+    return show(v_level, today, date_from, date_to, ago)
 
 def show(level=0, today=True, date_from=None, date_to=None, ago=None):
     """Show today's binder."""
     if today:
         today = str(datetime.date.today())
         binder = create_date_binder(today)
+    if (date_from is not None) or (date_to is not None):
+        if ago is not None:
+            binder = create_date_binder(date_from=date_from, offset=int(ago), date_to=date_to)
+        else:
+            binder = create_date_binder(date_from=date_from, offset=None, date_to=date_to)
     print(binder)
     items = []
     index = 0
