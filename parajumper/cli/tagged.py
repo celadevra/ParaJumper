c7ff456a8d94d1a04c8feb5178dc5a08bf387042
Haoyang Xu <haoyang@expoundite.net>
2016-12-20 16:02:46 +0800

    deal with verbosity and reuse item show method in day

--- parajumper/cli/tagged.py
+++ parajumper/cli/tagged.py
@@ -5,5 +5,20 @@ def dispatch(args):
     """Dispatcher for tagged command."""
     args.remove('tagged') 
     tags = args.not_flags.all
-    db.create_tag_binder(tags)
+    verbosity = args.value_after('-v')
+    tagged_binder(verbosity, *tags)
+
+def tagged_binder(verbosity=0, *tags):
+    """Generate and show binder populated with items with any of the tags."""
+    binder = db.create_tag_binder(tags)
+    print(binder)
+    items = []
+    index = 0
+    for member in binder.members:
+        items.append(load_item(member))
+    for item in items:
+        puts(colored.blue("[%2d]" % index))
+        print(item.__str__(verbose=level))
+        index += 1
+
 
